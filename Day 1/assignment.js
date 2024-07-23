635fb55a0a75201d556080b57d4af0599e1f1894
badru2 <hilalbadru4@gmail.com>
2024-07-23 12:00:40 +0700

    Change to backtip

--- Day 1/assignment.js
+++ Day 1/assignment.js
@@ -1,14 +1,14 @@
 // For Console
-var nama = "Hilal";
-var umur = 18;
-console.log("Nama Saya: " + nama + "\n" + "Umur Saya: " + umur);
+const nama = "Hilal";
+const umur = 18;
+console.log(`Nama Saya: ${nama} \nUmur Saya: ${umur}`);
 
 // For Web
 var http = require("http");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Nama Saya: " + nama + "<br>" + "Umur Saya: " + umur);
+    res.write(`Nama Saya: ${nama} <br> Umur Saya: ${umur}`);
     res.end();
   })
   .listen(8080);
