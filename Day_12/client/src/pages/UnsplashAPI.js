70beda1b03ee510e27ee9e9df7666d788f868588
badru2 <hilalbadru4@gmail.com>
2024-08-09 17:02:55 +0700

    Day 15

--- Day_12/client/src/pages/UnsplashAPI.js
+++ Day_12/client/src/pages/UnsplashAPI.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Masonry from "react-masonry-css";
 
 function UnsplashAPI() {
   const [img, setImg] = useState("");
@@ -20,16 +21,44 @@ function UnsplashAPI() {
     fetchRequest();
   }, [img]);
 
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
   return (
     <div>
-      <input
-        className="col-3 form-control-sm py-1 fs-4 text-capitalize border border-3 border-dark"
-        type="text"
-        placeholder="Search Anything..."
-        value={img}
-        onChange={(e) => setImg(e.target.value)}
-      />
-      <button type="submit">Submit</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchRequest();
+        }}
+        className="mb-4"
+      >
+        <input
+          className="col-3 form-control-sm py-1 fs-4 text-capitalize border border-3 border-dark"
+          type="text"
+          placeholder="Search Anything..."
+          value={img}
+          onChange={(e) => setImg(e.target.value)}
+        />
+        {/* <button type="submit">Submit</button> */}
+      </form>
+      <div>
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {res.map((item) => (
+            <div key={item.id} className="grid-item">
+              <img src={item.urls.small} alt={item.description} />
+            </div>
+          ))}
+        </Masonry>
+      </div>
     </div>
   );
 }
