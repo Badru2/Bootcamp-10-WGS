eb9d2c4b0bb626858607945f8f416f110d6cd37a
badru2 <hilalbadru4@gmail.com>
2024-08-12 13:41:16 +0700

    Day 16

---
+++ Day_16/client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import VideoGallery from "./pages/VideoGallery";
+
+function App() {
+  return (
+    <div className="App mx-4 max-w-7xl">
+      <h3>YouTube Video Gallery</h3>
+      <VideoGallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ Day_16/client/src/pages/VideoGallery.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const VideoGallery = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetchVideos("totk"); // Default search on component mount
+  }, []);
+
+  const fetchVideos = async (query) => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search`,
+        {
+          params: {
+            part: "snippet",
+            maxResults: 6,
+            key: process.env.REACT_APP_YOUTUBE_API_KEY,
+            q: query,
+          },
+        }
+      );
+      console.log(response.data.items); // Debugging line
+      setVideos(response.data.items);
+      setSelectedVideo(response.data.items[0]);
+    } catch (error) {
+      console.error("Error fetching videos:", error);
+    }
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    fetchVideos(searchTerm);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for videos"
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <div className="flex">
+        {selectedVideo && (
+          <div className="selected-video">
+            <iframe
+              width="960"
+              height="555"
+              src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}`}
+              frameBorder="0"
+              allowFullScreen
+              title={selectedVideo.snippet.title}
+            ></iframe>
+          </div>
+        )}
+
+        <div className="">
+          {videos.map((video) => (
+            <div
+              key={video.id.videoId}
+              className={`flex mb-3  ${
+                video.id.videoId === selectedVideo.id.videoId ? "active" : ""
+              }`}
+              onClick={() => setSelectedVideo(video)}
+            >
+              <img
+                src={video.snippet.thumbnails.default.url}
+                alt={video.snippet.title}
+                className=" object-cover"
+              />
+              <p className="w-[300px]">{video.snippet.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoGallery;
